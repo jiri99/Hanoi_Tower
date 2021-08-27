dafafe7630f8f6a831e726cb9a230e2437ded9d6
jiri99 <44276136+jiri99@users.noreply.github.com>
2021-08-27 13:27:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,29 +4,47 @@
 #include <QVBoxLayout>
 #include <QStackedWidget>
 #include <QSlider>
+#include <QPainter>
+#include <QStylePainter>
+#include <QLabel>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char **argv)
 {
- QApplication app (argc, argv);
+    QApplication app (argc, argv);
 
- QWidget window;
- window.setFixedSize(960, 540);
+    QWidget window;
+    window.setFixedSize(960, 540);
 
- QPushButton *button = new QPushButton("Run", &window);
+    QPushButton *button = new QPushButton("Run", &window);
 
- QVBoxLayout *mainlayout = new QVBoxLayout;
- QHBoxLayout *navlayout = new QHBoxLayout;
- QHBoxLayout *displayout = new QHBoxLayout;
+    QVBoxLayout *mainlayout = new QVBoxLayout;
+    QHBoxLayout *navlayout = new QHBoxLayout;
+    QHBoxLayout *displayout = new QHBoxLayout;
 
- QSlider *horizontalSliders = new QSlider(Qt::Horizontal);
+    QPainter painter;
+    painter.setPen(Qt::blue);
+    painter.setFont(QFont("Arial", 30));
+    painter.drawRect(QRect(80,120,200,100));
 
- navlayout->addWidget(horizontalSliders);
- navlayout->addWidget(button);
+    QGraphicsView * scene = new QGraphicsView;
 
- mainlayout->addLayout(displayout);
- mainlayout->addLayout(navlayout);
- window.setLayout(mainlayout);
+    QRectF r1(100.0, 200.1, 110.2, 106.3);
 
- window.show();
- return app.exec();
+    scene->setSceneRect(r1);
+
+    QSlider *horizontalSliders = new QSlider(Qt::Horizontal);
+
+    displayout->addWidget(scene);
+
+    navlayout->addWidget(horizontalSliders);
+    navlayout->addWidget(button);
+
+    mainlayout->addLayout(displayout);
+    mainlayout->addLayout(navlayout);
+    window.setLayout(mainlayout);
+
+    window.show();
+    return app.exec();
 }
